61b762d5048e82ba4e8c8e47d089df0cb04984f8
Successful72 <147023873+Successful72@users.noreply.github.com>
2025-05-02 17:09:51 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -8,18 +8,6 @@ echo "Include Docker: $INCLUDE_DOCKER"
 echo "Create pppoe-settings"
 mkdir -p  /home/build/immortalwrt/files/etc/config
 
-# 不需要参与编译的软件包
-# PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
-# PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-# PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
-# PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
-# PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
-# PACKAGES="$PACKAGES luci-app-openclash"
-
-# 增加几个必备组件 方便用户安装iStore
-# PACKAGES="$PACKAGES fdisk"
-# PACKAGES="$PACKAGES script-utils"
-
 # 创建pppoe配置文件 yml传入环境变量ENABLE_PPPOE等 写入配置文件 供99-custom.sh读取
 cat << EOF > /home/build/immortalwrt/files/etc/config/pppoe-settings
 enable_pppoe=${ENABLE_PPPOE}
@@ -32,9 +20,7 @@ cat /home/build/immortalwrt/files/etc/config/pppoe-settings
 # 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 
-
-
-# 定义所需安装的包列表 下列插件你都可以自行删减
+# 定义所需安装的软件列表 下列插件可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
@@ -44,6 +30,21 @@ PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 #24.10
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
+
+# 不需要参与编译的软件包
+# PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+# PACKAGES="$PACKAGES luci-app-openclash"
+# 增加几个必备组件 方便用户安装iStore
+# PACKAGES="$PACKAGES fdisk"
+# PACKAGES="$PACKAGES script-utils"
+
+# 若有未列出的软件，前往下面的网站，自行搜索。能搜到就说明能安装。根据上面的格式增加代码即可。
+# https://mirrors.sjtug.sjtu.edu.cn/immortalwrt/releases/
+
 
 
 # 判断是否需要编译 Docker 插件
