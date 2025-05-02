6ab70d3c39e29874c50e03db1dbcc30340504cac
Successful72 <147023873+Successful72@users.noreply.github.com>
2025-05-02 17:10:20 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -20,6 +20,8 @@ cat /home/build/immortalwrt/files/etc/config/pppoe-settings
 # 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 
+
+
 # 定义所需安装的软件列表 下列插件可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
